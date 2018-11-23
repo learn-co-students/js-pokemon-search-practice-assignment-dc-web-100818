39a171665856d99ab927a26a71506422331e6b5f
github username <github email address>
2018-11-23 07:28:32 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
-  //YOUR CODE HERE
+   //creates html
+  const pokeMonCardHTML = POKEMON.map(pObj => (
+
+  	`<div class="pokemon-container">
+  	  <div style="width:200px;margin:10px;background:#yellow;color:#2d72fc" class="pokemon-frame">
+  	   <h1 class="center-text">${pObj.name}</h1>
+  	    <div style="width:200px;margin:auto">
+  	     <img data-id="{pObj.id}" data-action="flip" class="toggle-sprite" src="${pObj.sprites.front}">
+  	     </div>
+  	    </div>
+  	    </div>
+  	   </div>`
+  	 )).join('')
+
+  	document.getElementById('pokemon-container').innerHTML = pokeMonCardHTML
+
+   document.getElementById('pokemon-container').addEventListener('click', event => {
+    if (event.target.dataset.action === 'flip') {
+      const targetPoke = POKEMON.find(pObj => pObj.id == event.target.dataset.id)
+      if (event.target.src === targetPoke.sprites.front) {
+        event.target.src = targetPoke.sprites.back
+      } else {
+        event.target.src = targetPoke.sprites.front
+      }
+    }
+  })
+
+  	document.getElementById('pokemon-search-form').addEventListener('input', event => {
+    const filterPokes = POKEMON.filter(pObj => pObj.name.includes(event.target.value.toLowerCase()))
+    const filterPokeHTML = filterPokes.map(pObj => (
+      `<div class="pokemon-container">
+        <div style="width:200px;margin:10px;background:#yellow;color:#2d72fc" class="pokemon-frame">
+          <h1 class="center-text">${pObj.name}</h1>
+          <div style="width:200px;margin:auto">
+            <div style="width:90px;margin:auto">
+              <img data-id="${pObj.id}" data-action="flip" class="toggle-sprite" src="${pObj.sprites.front}">
+            </div>
+          </div>
+        </div>
+      </div>`
+      )).join('')
+
+    document.getElementById('pokemon-container').innerHTML = filterPokeHTML.length ? filterPokeHTML : `<p><center>Now there are pokemon</center></p>`
+  })
 })
+
+
+
+
+
+
+
+
+
+  	
