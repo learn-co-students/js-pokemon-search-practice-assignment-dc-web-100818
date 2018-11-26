a0516f207d88c8c61bd9f4264029280c4fa7384b
Joseph Arias <arias.joseph.a@gmail.com>
2018-11-25 19:15:03 -0500

    JS Pokemon Search Practice Assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,86 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
-  //YOUR CODE HERE
-})
+  const allPokemonData = []
+  let container = document.getElementById('pokemon-container')
+  const searchInput = document.getElementById('pokemon-search-input')
+
+  // event listeners
+  searchInput.addEventListener('keyup', filterPokemon)
+  container.addEventListener('click', flipPokemon)
+  
+  getPokemonApi();
+
+  function getPokemonApi() {
+    fetch('http://localhost:3000/pokemon')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+     data.forEach(function(pokemon) {
+       allPokemonData.push(pokemon)
+      let pokemonCard = new Pokemon(pokemon.name, pokemon.sprites.front, pokemon.sprites.back, pokemon.id)
+      pokemonCard.render(container)
+     })
+    });
+  }
+
+  function filterPokemon() {
+    container.innerHTML = '';
+    let filterList = allPokemonData.filter(pokemon => {
+      return pokemon.name.includes(this.value.toLowerCase())
+    });
+    filterList.forEach(pokemon => {
+      let filterCard = new Pokemon(pokemon.name, pokemon.sprites.front, pokemon.sprites.back, pokemon.id)
+      filterCard.render(container)
+    })
+  }
+
+  function flipPokemon(e) {
+    if (e.target.className.includes('flip-image')) {
+      let pokemonSrc = e.target.parentElement.children[1].children[0].src
+      let pokemonId = e.target.parentElement.id
+      let foundPokemon = allPokemonData.find(pokemon => {
+        if (pokemon.id == pokemonId) {
+          return pokemon;
+        }
+      }); 
+    
+      if (pokemonSrc == foundPokemon.sprites.front) {
+        pokemonSrc = foundPokemon.sprites.back
+        e.target.parentElement.children[1].children[0].src = pokemonSrc
+      } else if (pokemonSrc == foundPokemon.sprites.back) {
+        pokemonSrc = foundPokemon.sprites.front
+        e.target.parentElement.children[1].children[0].src = pokemonSrc
+      }
+    }
+  }
+
+});
+
+//YOUR CODE HERE
+  class Pokemon {
+    constructor(name, front, back, id) {
+      this.name = name;
+      this.front = front;
+      this.back = back;
+      this.id = id;
+    }
+
+    render(element) {
+      
+      let div = document.createElement('div');
+      div.className = 'pokemon-container'
+      div.innerHTML = `
+      <div class="pokemon-frame" id="${this.id}">
+        <h1 class="center-text">${this.name}</h1>
+        <div class="${this.name}-image">
+          <img src="${this.front}">
+        </div>
+        <p class="center-text flip-image">flip card</p>
+      </div>
+      `;
+      element.appendChild(div)
+     
+    }
+  }
+
+
